5716f4b82e24e58b947683834e84c824f19b2cff
Malik <malik.hassanaly@gmail.com>
2023-09-26 09:52:46 -0600

    dont use verbose argument in the example and link non final models in case of early plotting

--- pinn_spm_param/postProcess/exec.sh
+++ pinn_spm_param/postProcess/exec.sh
@@ -1,15 +1,15 @@
 dataFolder=../integration_spm/
-modelFolder=../ModelFin_1
-logFolder=../LogFin_1
+modelFolder=../Model_1
+logFolder=../Log_1
 
 # Plot correlation
 python plotData.py -nosimp -df $dataFolder -p 0.5 1
 
 # Plot correlation
-python plotCorrelationPINNvsData.py -v -nosimp -mf $modelFolder -df $dataFolder -p 0.5 1
+python plotCorrelationPINNvsData.py -nosimp -mf $modelFolder -df $dataFolder -p 0.5 1
 
 # Plot contour of PINN prediction
-python plotPINNResult.py -v -nosimp -mf $modelFolder -p 0.5 1
+python plotPINNResult.py -nosimp -mf $modelFolder -p 0.5 1
 
 python plotPINNResult_movie.py -nosimp -mf $modelFolder -df $dataFolder -p 0.5 1
 
